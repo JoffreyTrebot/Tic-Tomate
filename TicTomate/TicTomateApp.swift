3453814d7e11ecfb58ce2a4f250c93e418fea77c
Joffrey Trebot <trebotjoffrey@gmail.com>
2024-12-31 01:29:42 +0100

    Initial Commit

---
+++ TicTomate/TicTomateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTomateApp.swift
+//  TicTomate
+//
+//  Created by Joffrey Trébot on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTomateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTomateTests/TicTomateTests.swift
@@ -0,0 +1,16 @@
+//
+//  TicTomateTests.swift
+//  TicTomateTests
+//
+//  Created by Joffrey Trébot on 12/31/24.
+//
+
+import Testing
+
+struct TicTomateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
